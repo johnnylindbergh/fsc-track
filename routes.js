bc33b9fce03dd510cdc84b0d1d0a3f6036a11f60
Johnny Lindbergh <johnnyl8@embarqmail.com>
2023-09-06 12:16:58 -0400

    clean

--- routes.js
+++ routes.js
@@ -81,10 +81,8 @@ module.exports = function(app) {
   });
 
    app.get('/qr/:jobId/:taskId/', mid.isAuth, (req, res) => {
-    console.log("QR code scanned")
     var render = defaultRender(req);
     if (req.isAuthenticated() && req.user && req.user.local) {
-      console.log(req.user.local.id, req.params.jobId, req.params.taskId);
       db.clockInAndOut(req.user.local.id, req.params.jobId, req.params.taskId, function(err){
         if (!err){
           res.redirect('/');
@@ -116,7 +114,6 @@ module.exports = function(app) {
                db.lookUpUser(userEmail, function(err, rows){
 
                   render.clockedIn = rows.clockedIn;
-                  //console.log(render);
                   //res.send(render)
                   res.render("main.html", render);
               });
@@ -134,7 +131,6 @@ module.exports = function(app) {
 app.post('/addJob',mid.isAuth, function (req, res){
         // start with default render object
         var render = defaultRender(req);
-        //console.log(req.body.jobName);
         // ensure given name exists
         if (req.body.jobName) {
                 // create new job entry in DB
@@ -235,27 +231,23 @@ app.post('/searchTimesheet', mid.isAuth, function(req, res){
 
               for (var i = 0; i < render.users.length; i++){
                 if (render.users[i].id == req.body.userId){
-                  console.log("selected")
                   render.users[i].selected = true;
                 }
               }
               
               for (var i = 0; i < render.jobs.length; i++){
                 if (render.jobs[i].id == req.body.jobId){
-                                    console.log("selected")
 
                   render.jobs[i].selected = true;
                 }
               }
                 for (var i = 0; i < render.tasks.length; i++){
                 if (render.tasks[i].id == req.body.taskId){
-                                    console.log("selected")
 
                   render.tasks[i].selected = true;
                 }
               }
 
-              console.log("filtering timesheet")
               
               // parse dates from request into moment objects
               var startDate = moment(req.body.startDate);
@@ -272,7 +264,6 @@ app.post('/searchTimesheet', mid.isAuth, function(req, res){
               if (req.body.taskId == -1){
                             req.body.taskId = null;
               }
-              console.log(req.body)
 
               db.getTimesheetQuery(req, res, startDate, endDate, req.body.userId, req.body.jobId, req.body.taskId,  function(err,rows){
                 if (!err && rows.length > 0){
@@ -284,7 +275,6 @@ app.post('/searchTimesheet', mid.isAuth, function(req, res){
 
 
 
-                console.log(render);
                 res.render("admin.html", render);
               }); 
 
@@ -327,27 +317,23 @@ app.post('/searchTimesheetToCSV', mid.isAuth, function(req, res){
 
               for (var i = 0; i < render.users.length; i++){
                 if (render.users[i].id == req.body.userId){
-                  console.log("selected")
                   render.users[i].selected = true;
                 }
               }
               
               for (var i = 0; i < render.jobs.length; i++){
                 if (render.jobs[i].id == req.body.jobId){
-                                    console.log("selected")
 
                   render.jobs[i].selected = true;
                 }
               }
                 for (var i = 0; i < render.tasks.length; i++){
                 if (render.tasks[i].id == req.body.taskId){
-                                    console.log("selected")
 
                   render.tasks[i].selected = true;
                 }
               }
 
-              console.log("filtering timesheet")
               
               // parse dates from request into moment objects
               var startDate = moment(req.body.startDate);
@@ -364,7 +350,6 @@ app.post('/searchTimesheetToCSV', mid.isAuth, function(req, res){
               if (req.body.taskId == -1){
                             req.body.taskId = null;
               }
-              console.log(req.body)
 
               db.getTimesheetQuery(req, res, startDate, endDate, req.body.userId, req.body.jobId, req.body.taskId,  function(err,rows){
                 if (!err && rows.length > 0){
@@ -376,7 +361,6 @@ app.post('/searchTimesheetToCSV', mid.isAuth, function(req, res){
 
                 
 		fs.writeFile('timesheet.csv',arrayToCSV(rows), function(){
-			console.log(err)
 			res.download('timesheet.csv');
 		});
               }); 
@@ -399,34 +383,6 @@ app.post('/searchTimesheetToCSV', mid.isAuth, function(req, res){
       }
 
  });
-
-//    console.log(req._json);
- //   db.getJobs(req, res, function (err, rows){
-//      console.log(rows);
- //     res.render("sample.html",{jobs:rows});
- //   });
-
-  /*
-    ************************************
-    *                                  *
-    *   Routes go here...              *
-    *                                  *
-    *   app.get('/', (req, res) => {   *
-    *     ...                          *
-    *   });                    
-app.get('/getRAlocationStats', (req, res) => {
-   // renderMessages(req,res)
-   db.getRAlocationStats(req, res, function (loc){
-      console.log(loc);
-      res.render('locations.html', {locations:loc});
-   });
-
-
-});        *
-    *                                  *
-    ************************************
-  */
-
 }
 
 function arrayToCSV(objArray) {
