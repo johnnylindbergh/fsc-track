c2086743925ecec58ed300068a45298f8e02623a
Johnny Lindbergh <johnnyl8@embarqmail.com>
2025-03-04 12:02:49 -0500

    debugging

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ module.exports = {
   DB_NAME: 'fscTrack',
 
   // domain through which server is accessible
-  DOMAIN: 'https://fsc.johnnylindbergh.com',
+  DOMAIN: 'https://shop.franksaulconstruction.com',
 
   /*  does the system allow automatic creation of new user accounts
       when authentication is attempted. */
